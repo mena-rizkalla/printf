bba073bfae8a547e4bf67b2f642ae4f0b9576929
mena-rizkalla <mena.rizkalla1@gmail.com>
2023-03-27 13:53:44 -0700

    main

--- main.h
+++ main.h
@@ -38,8 +38,6 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
-
-
 int print_char(va_list types, char buffer[],
         int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
@@ -69,12 +67,10 @@ int print_reverse(va_list types, char buffer[],
 int print_rot13string(va_list types, char buffer[],
         int flags, int width, int precision, int size);
 
-
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
-
 
 int handle_write_char(char c, char buffer[],
         int flags, int width, int precision, int size);
@@ -86,7 +82,6 @@ int write_pointer(char buffer[], int ind, int length,
         int width, int flags, char padd, char extra_c, int padd_start);
 int write_unsgnd(int is_negative, int ind, char buffer[],
         int flags, int width, int precision, int size);
-
 
 int is_printable(char);
 int append_hexa_code(char, char[], int);
